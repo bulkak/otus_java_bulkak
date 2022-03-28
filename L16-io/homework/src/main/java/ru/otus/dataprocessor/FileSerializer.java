d756d57df0867d7ee204ca4e0818a161192a1c33
denis.derkach <vQbaS1DITuA^!>
2022-03-29 00:05:49 +0300

    HW-16 IO, Serialization

---
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -0,0 +1,27 @@
+package ru.otus.dataprocessor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FileSerializer implements Serializer {
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final String fileName;
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+    @Override
+    public void serialize(Map<String, Double> data) {
+        //формирует результирующий json и сохраняет его в файл
+        var file = new File(fileName);
+        try {
+            mapper.writeValue(file, data);
+        } catch (IOException e) {
+            throw new FileProcessException("Couldn't write to file!");
+        }
+    }
+}
